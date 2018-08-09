98b71d36a64d6f272f16a459aaba180bf57092bd
zhuxudong <callzhuxudong@163.com>
2018-08-09 15:33:54 +0800

    update doc

--- src/client.js
+++ src/client.js
@@ -1,14 +1,6 @@
-// let io = require('socket.io-client');
-// import "babylonjs"
-//
-// declare let window: any
-//
-// let socket = io('ws://localhost:3000/socket.io');
-// window.socket = socket
-// window.io = io
-import "@/tool/reset";
-import {editMesh, unEditMesh, editMaterial, unEditMaterial} from "@/tool/edit"
-import initSceneByJSON from '@/tool/initSceneByJSON';
+import "./components/tool/reset";
+import {editMesh, unEditMesh, editMaterial, unEditMaterial} from "./components/tool/edit"
+import initSceneByJSON from './components/tool/initSceneByJSON';
 
 /** 调用openDebug()开启调试
  *  不开启的时候文档不会加载相关文件,节省线上资源*/
--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,3 +1,11 @@
+// let io = require('socket.io-client');
+// import "babylonjs"
+//
+// declare let window: any
+//
+// let socket = io('ws://localhost:3000/socket.io');
+// window.socket = socket
+// window.io = io
 module.exports = () => {
 
 }
